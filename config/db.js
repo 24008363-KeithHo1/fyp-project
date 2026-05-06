84aae6dbce0cbb0375c5fb5465658c4907b5f02c
Tasty465 <dariussun2@gmail.com>
2026-05-06 14:01:13 +0800

    Updated Env and db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'fyp', process.env.DB_USER || 'root', process.env.DB_PASS || '', {
+const sequelize = new Sequelize(process.env.DB_NAME || 'fyp', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '', {
   host: process.env.DB_HOST || 'localhost',
   dialect: 'mysql',
   logging: false,
