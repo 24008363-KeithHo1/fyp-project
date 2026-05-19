b555b9b95cf783671ea7782cd1d0b12b95289f40
Tasty465 <dariussun2@gmail.com>
2026-05-19 22:06:13 +0800

    Added Bank Reconcile + payments page with routing for finance

--- routes/finance.js
+++ routes/finance.js
@@ -10,4 +10,12 @@ router.get('/dashboard', (req, res) => {
   res.render('finance/dashboard');
 });
 
+router.get('/payments', (req, res) => {
+  res.render('finance/payments');
+});
+
+router.get('/bank-reconcile', (req, res) => {
+  res.render('finance/bank-reconcile');
+});
+
 module.exports = router;
